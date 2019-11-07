a6921d8256c75a72965ac0e106e84659ae1d73da
Timotej Zaťko <timi.zatko@gmail.com>
2019-11-07 23:05:41 +0100

    add gui

--- app/main.js
+++ app/main.js
@@ -1,16 +1,16 @@
 /**
  * @type {JQueryStatic}
  */
-const $ = jQuery
+const $ = jQuery;
 
-let clipboardData = ''
+let clipboardData = "";
 
 function getClipboardContent(event) {
-  const clipboardContent = event.clipboardData.getData('text/html')
-  event.clipboardData.clearData()
-  event.preventDefault()
+  const clipboardContent = event.clipboardData.getData("text/html");
+  event.clipboardData.clearData();
+  event.preventDefault();
 
-  return clipboardContent
+  return clipboardContent;
 }
 
 /**
@@ -25,76 +25,96 @@ function getClipboardContent(event) {
  * @example
  *   requestHtml('Rekonstruovat')
  */
-async function requestHtml(text, method='4gram') {
-  const fetchParams = new URLSearchParams()
-  fetchParams.set('method', method)
-  fetchParams.set('text', text)
+async function requestHtml(text, method = "4gram") {
+  const fetchParams = new URLSearchParams();
+  fetchParams.set("method", method);
+  fetchParams.set("text", text);
 
   const fetchInit = {
-    method: 'POST',
-    mode: 'cors',
+    method: "POST",
+    mode: "cors",
     body: fetchParams
-  }
+  };
 
-  let fetchRequest = new Request('https://cors-anywhere.herokuapp.com/https://diakritik.juls.savba.sk');
+  let fetchRequest = new Request(
+    "https://cors-anywhere.herokuapp.com/https://diakritik.juls.savba.sk"
+  );
 
-  const response = await fetch(fetchRequest, fetchInit)
-  const htmlString = await response.text()
+  const response = await fetch(fetchRequest, fetchInit);
+  const htmlString = await response.text();
 
-  return htmlString
+  return htmlString;
 }
 
 function extractOriginalText(htmlString) {
-  const tableDataElements = $(htmlString).find('td')
+  const tableDataElements = $(htmlString).find("td");
 
-  const textArray = []
+  const textArray = [];
   tableDataElements.each((i, e) => {
-    const trimmedText = e.innerText.trim()
+    const trimmedText = e.innerText.trim();
 
-    if (trimmedText !== '') {
-      textArray.push(trimmedText)
+    if (trimmedText !== "") {
+      textArray.push(trimmedText);
     }
-  })
+  });
 
-  const textString = textArray.join(' !@#$#@! ')
+  const textString = textArray.join(" !@#$#@! ");
 
-  return textString
+  return textString;
 }
 
 function extractCheckedText(htmlString) {
-  let checkedText = $(htmlString).find('div.recinside').text()
+  let checkedText = $(htmlString)
+    .find("div.recinside")
+    .text();
 
-  checkedText = checkedText
-    .replace(/ !@#\$#@! /gmi, '\n')
+  checkedText = checkedText.replace(/ !@#\$#@! /gim, "\n");
 
-  return checkedText
+  return checkedText;
 }
 
 function setClipboardContent(event, checkedText) {
-  console.log(checkedText)
-  // navigator.clipboard.writeText(checkedText)
-  event.clipboardData.setData('text/plain', checkedText)
+  event.clipboardData.setData("text/plain", checkedText);
 
   // EXP: Setting clipboard content in Google Chrome does not work without
-  //      the following line.
-  event.preventDefault()
+  // the following line.
+  event.preventDefault();
 }
 
-document.addEventListener('paste', async (event) => {
+function setStatus(text) {
+  document.getElementById("appStatus").innerHTML = text;
+
+  exampleImg.hide();
+}
+
+const exampleImg = $("img.example");
+
+document.addEventListener("paste", async event => {
   const originalHtmlString = getClipboardContent(event);
-  const originalText = extractOriginalText(originalHtmlString)
-  const htmlString = await requestHtml(originalText)
-  const checkedText = extractCheckedText(htmlString)
+  const originalText = extractOriginalText(originalHtmlString);
 
-  clipboardData = checkedText
+  setStatus("Loading...");
 
-  console.log('Checked!')
-})
+  const htmlString = await requestHtml(originalText);
+  const checkedText = extractCheckedText(htmlString);
 
-document.addEventListener('copy', async (event) => {
+  clipboardData = checkedText;
+
+  setStatus("Now copy... (Ctrl + C)");
+});
+
+document.addEventListener("copy", async event => {
   setClipboardContent(event, clipboardData);
 
-  clipboardData = ''
+  clipboardData = "";
 
-  console.log('Copied!')
-})
+  setStatus(
+    "Copied into clipboard! Now paste the columns back to the spreadsheet. You can also do Ctrl + V again."
+  );
+  exampleImg.show();
+});
+
+setStatus(
+  "Copy the columns from Google Spreadsheet and paste them here. (Ctrl + V)"
+);
+exampleImg.show();
