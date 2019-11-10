5d5731a4cdd9667e958a1384b3195bd1ff4e7dc4
Pavel Balashov <pavel.balashov1@gmail.com>
2019-11-10 13:55:58 +0100

    Finilize the approach

--- app/main.js
+++ app/main.js
@@ -2,15 +2,21 @@
  * @type {JQueryStatic}
  */
 const $ = jQuery;
+/**
+ * @type {import("lodash").LoDashStatic}
+ */
+const lodash = _
+_ = lodash
+delete lodash
 
 let clipboardData = "";
 
 function getClipboardContent(event) {
-  const clipboardContent = event.clipboardData.getData("text/html");
-  event.clipboardData.clearData();
-  event.preventDefault();
+    const clipboardContent = event.clipboardData.getData("text/html");
+    event.clipboardData.clearData();
+    event.preventDefault();
 
-  return clipboardContent;
+    return clipboardContent;
 }
 
 /**
@@ -26,95 +32,140 @@ function getClipboardContent(event) {
  *   requestHtml('Rekonstruovat')
  */
 async function requestHtml(text, method = "4gram") {
-  const fetchParams = new URLSearchParams();
-  fetchParams.set("method", method);
-  fetchParams.set("text", text);
+    const fetchParams = new URLSearchParams();
+    fetchParams.set("method", method);
+    fetchParams.set("text", text);
 
-  const fetchInit = {
-    method: "POST",
-    mode: "cors",
-    body: fetchParams
-  };
+    const fetchInit = {
+        method: "POST",
+        mode: "cors",
+        body: fetchParams
+    };
 
-  let fetchRequest = new Request(
-    "https://cors-anywhere.herokuapp.com/https://diakritik.juls.savba.sk"
-  );
+    let fetchRequest = new Request(
+        "https://cors-anywhere.herokuapp.com/https://diakritik.juls.savba.sk"
+    );
 
-  const response = await fetch(fetchRequest, fetchInit);
-  const htmlString = await response.text();
+    const response = await fetch(fetchRequest, fetchInit);
+    const htmlString = await response.text();
 
-  return htmlString;
+    return htmlString;
 }
 
 function extractOriginalText(htmlString) {
-  const tableDataElements = $(htmlString).find("td");
+    const tableRowsJquery = $("<div />").append(htmlString).find("tr");
 
-  const textArray = [];
-  tableDataElements.each((i, e) => {
-    const trimmedText = e.innerText.trim();
+    // const rowCount = tableRows.length
+    // const columnCount = tableRows.slice(0, 1).find("td").length
 
-    if (trimmedText !== "") {
-      textArray.push(trimmedText);
+    const tableRowsArray = []
+    tableRowsJquery.each((tableRowIndex, tableRowElement) => {
+        const rowArray = []
+        const cellsJquery = $("<div />").append(tableRowElement).find("td")
+        cellsJquery.each((cellIndex, cellElement) => {
+            const cellText = cellElement
+                .innerHTML
+                // .slice(1, -1)
+                .replace(/<br>/gim, " \\n ")
+                // .replace(/\\n/gim, "\n")
+            rowArray.push(cellText)
+        })
+
+        tableRowsArray.push(rowArray)
+    })
+
+    // const transposedTableRowsArray = _.zip(...tableRowsArray)
+    /**
+     * @type {Array<Array<String>>}
+     */
+    const transposedTableRowsArray = _.zip.apply(_, tableRowsArray)
+
+    let textString = ""
+    for (let columnCellsArray of transposedTableRowsArray) {
+        columnCellsArray = columnCellsArray.filter(
+            (columnCellString) => {
+                return columnCellString.trim() !== ""
+            }
+        )
+
+        listLabelHtmlString = "<p>" + columnCellsArray[0] + "</p>"
+        listEntriesArray = columnCellsArray.slice(1)
+        const listEntriesHtmlArray = listEntriesArray.map((columnCellString, ) => {
+            return "<li>" + columnCellString + "</li>"
+        })
+
+        const listEntriesHtmlString = listEntriesHtmlArray.join("")
+        const divListJquery = $("<div />").append("<ul />")
+        const listJquery = divListJquery.find("ul").append(listEntriesHtmlString)
+
+        textString += listLabelHtmlString + divListJquery.html()
     }
-  });
 
-  const textString = textArray.join(" !@#$#@! ");
+    textString = textString
+        .replace(/p>/gim, "@>")
+        .replace(/ul>/gim, "$>")
+        .replace(/li>/gim, "#>")
 
-  return textString;
+    return textString;
 }
 
-function extractCheckedText(htmlString) {
-  let checkedText = $(htmlString)
-    .find("div.recinside")
-    .text();
+function extractCheckedTextHtml(htmlString) {
+    let checkedTextHtml = $(htmlString)
+        .find("div.recinside")
+        .text()
+        .replace(/@>/gim, "p>")
+        .replace(/\$>/gim, "ul>")
+        .replace(/#>/gim, "li>")
 
-  checkedText = checkedText.replace(/ !@#\$#@! /gim, "\n");
-
-  return checkedText;
+    return checkedTextHtml;
 }
 
 function setClipboardContent(event, checkedText) {
-  event.clipboardData.setData("text/plain", checkedText);
+    event.clipboardData.setData("text/html", checkedText);
 
-  // EXP: Setting clipboard content in Google Chrome does not work without
-  // the following line.
-  event.preventDefault();
+    // EXP: Setting clipboard content in Google Chrome does not work without
+    // the following line.
+    event.preventDefault();
 }
 
 function setStatus(text) {
-  document.getElementById("appStatus").innerHTML = text;
+    document.getElementById("appStatus").innerHTML = text;
 
-  exampleImg.hide();
+    exampleImg.hide();
 }
 
 const exampleImg = $("img.example");
 
 document.addEventListener("paste", async event => {
-  const originalHtmlString = getClipboardContent(event);
-  const originalText = extractOriginalText(originalHtmlString);
+    const originalHtmlString = getClipboardContent(event);
+    const originalTextTranslationForm = extractOriginalText(originalHtmlString);
 
-  setStatus("Loading...");
+    console.log(originalTextTranslationForm)
 
-  const htmlString = await requestHtml(originalText);
-  const checkedText = extractCheckedText(htmlString);
+    setStatus("Loading...");
 
-  clipboardData = checkedText;
+    const htmlString = await requestHtml(originalTextTranslationForm);
+    const checkedTextHtml = extractCheckedTextHtml(htmlString);
 
-  setStatus("Now copy... (Ctrl + C)");
+    console.log(checkedTextHtml)
+
+    clipboardData = checkedTextHtml;
+
+    setStatus("Now copy... (Ctrl + C)");
 });
 
 document.addEventListener("copy", async event => {
-  setClipboardContent(event, clipboardData);
+    setClipboardContent(event, clipboardData);
 
-  clipboardData = "";
+    clipboardData = "";
 
-  setStatus(
-    "Copied into clipboard! Now paste the columns back to the spreadsheet. You can also do Ctrl + V again."
-  );
-  exampleImg.show();
+    setStatus(
+        "Copied into clipboard! Now paste the columns back to the spreadsheet. You can also do Ctrl + V again."
+    );
+    exampleImg.show();
 });
 
 setStatus(
-  "Copy the columns from Google Spreadsheet and paste them here. (Ctrl + V)"
+    "Copy the columns from Google Spreadsheet and paste them here. (Ctrl + V)"
 );
 exampleImg.show();
